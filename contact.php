097b2e72f6aeb3f603fdad6af06631f1575b870f
Stuart <stuart@Stuarts-MacBook-Air.local>
2019-04-02 16:50:53 +1000

    add images & blog update

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = 'contact form <contact@domain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'contact form <embracehealinghands@gmail.com>';
+$sendTo = 'contact form <becdkahunamassage@outlook.com>';
 
 // subject of the email
 $subject = 'New message from your contact form';
